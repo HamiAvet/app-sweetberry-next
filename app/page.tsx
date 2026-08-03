db468c1efd1bf17f7948049748796f020d9ee201
HamiAvet <hamov2003@gmail.com>
2026-08-03 16:13:10 +0200

    added test api

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,34 @@
 "use client";
 
+import { FormEvent } from "react";
+
 export default function Home() {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+
+    const response = await fetch("/api/test", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    console.log("data", data);
+  };
   return (
-    <main className="flex min-h-screen items-center justify-center">
+    <main className="flex flex-col min-h-screen items-center justify-center bg-[#d15c5c]">
         <h1 className="text-5xl font-bold text-blue-600">
             Work in progress !
         </h1>
+        <form onSubmit={handleSubmit} className="mt-4 flex flex-col items-center gap-2">
+            <input type="text" name="name" placeholder="Enter your name" />
+            <button type="submit">Submit</button>
+            <p className="mt-2 text-sm text-gray-600">
+                
+            </p>
+        </form>
     </main>
   );
 }
